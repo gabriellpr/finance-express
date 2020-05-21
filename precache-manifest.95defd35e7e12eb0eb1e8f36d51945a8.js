da3be906c4883c5f81bad5da08acaa908d6c39f5
gabriellpr <gabrielporfirio1994@gmail.com>
2020-05-20 23:58:12 -0300

    Updates

---
+++ precache-manifest.95defd35e7e12eb0eb1e8f36d51945a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56bff9a907f86505a2557f840aa2cdbb",
+    "url": "/finance-express/index.html"
+  },
+  {
+    "revision": "7a254501759ab909189b",
+    "url": "/finance-express/static/css/main.99c3382f.chunk.css"
+  },
+  {
+    "revision": "9873ff0ca33211fbde23",
+    "url": "/finance-express/static/js/2.f4b72132.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/finance-express/static/js/2.f4b72132.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a254501759ab909189b",
+    "url": "/finance-express/static/js/main.310b4fdd.chunk.js"
+  },
+  {
+    "revision": "4e8856c3b93fe6d73c9a",
+    "url": "/finance-express/static/js/runtime-main.9d908dfc.js"
+  }
+]);
